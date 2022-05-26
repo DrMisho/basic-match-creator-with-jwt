feaa1d71eec875499c72ba7e5bc4188ba88e8331
Mustafa Misho <mstafa.misho@gmail.com>
2022-05-26 22:12:25 +0300

    final

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ io.on('connection', (socket) => {
     })
   });
   
-const port = process.env.port || 3000 ;
+const port = process.env.PORT || 3000 ;
 server.listen(port, ()=>{
     console.log(`server is running on port ${port}`);
 })
